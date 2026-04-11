08e9ba4f8b6abbb40f6227019d762558841a6487
Faizan Aslam <m.faizanaslam10@gmail.com>
2026-04-12 00:27:03 +0500

    logo

--- components/Preloader.js
+++ components/Preloader.js
@@ -2,7 +2,7 @@ export default function Preloader() {
   return (
     <div id="de-loader">
       <div className="loader-content">
-        <img src="/assets/images/center-logo-shield.png" alt="Logo" width="321" height="275" />
+        <img src="/assets/images/loadinglogo.png" alt="Logo" width="321" height="275" />
         <div className="lds-roller">
           <div></div><div></div><div></div><div></div>
           <div></div><div></div><div></div><div></div>
